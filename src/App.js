696c8cd07ad290b11be733ceb38bb0087538fb1a
pkieber <113725311+pkieber@users.noreply.github.com>
2023-09-03 19:33:20 +0200

    add darkmode

--- src/App.js
+++ src/App.js
@@ -1,52 +1,110 @@
 import "./App.css";
 import { UserProvider, useUser } from "./UserContext";
+import { ThemeProvider, useTheme } from "./ThemeContext";
+import Switch from "./Switch";
 
 
 const LoggedInUser = () => {
   const {user} = useUser();
+  const { theme } = useTheme();
   return (
-    <p>
+    <p
+      style={{color: theme === "light" ? "black" : "white"}}
+    >
       Hello <span className="Username">{user.name}</span>
     </p>
   )
 }
 
-const Header = () => {
+
+const Title = ({ children }) => {
+  const { theme } = useTheme();
   return (
-      <header>
-        <h2>Little Lemon 🍕</h2>
-        <LoggedInUser />
-      </header>
+    <h2
+      style={{color: theme === "light" ? "black" : "white"}}
+    >
+      {children}
+    </h2>
   );
 };
 
-const Page = () => {
+
+const Paragraph = ({ children }) => {
+  const { theme } = useTheme();
+  return (
+    <p
+      style={{color: theme === "light" ? "black" : "white"}}
+    >
+      {children}
+    </p>
+  );
+};
+
+
+const Content = () => {
   const {user} = useUser();
   return (
+    <div className="content-container">
+      <Paragraph> 
+          <p>
+            We are a pizza loving family. And for years, I searched and searched and
+            searched for the perfect pizza dough recipe. I tried dozens, or more.
+            And while some were good, none of them were that recipe that would
+            make me stop trying all of the others.
+          </p>
+          <p> 
+            Written by {user.name}
+          </p>
+      </Paragraph>
+    </div>
+  );
+};
+
+
+const Header = () => {
+  return (
     <div>
-      <h2>When it comes to dough</h2>
-      <p> Lorem lorem lorem </p>
-      <p> Written by {user.name} </p>
+      <header>
+        <Title>Little Lemon 🍕</Title>
+        <Switch />
+      </header>
+      <LoggedInUser />
+    </div>
+  );
+};
+
+
+const Page = () => {
+  return (
+    <div className="Page">
+      <Title>When it comes to dough</Title>
+      <Content />
     </div>
   );
 };
 
 
 function App() {
+  const { theme } = useTheme();
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{backgroundColor: theme === "light" ? "white" : "black"}}
+    >      
       <Header />
       <Page />
     </div>
   )
-
 }
+
 
 function Root() {
   return (
-    <UserProvider>
+    <ThemeProvider>
+      <UserProvider>
         <App />
-    </UserProvider>
+      </UserProvider>
+    </ThemeProvider>
   );
 }
 
